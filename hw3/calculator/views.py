1dc9f53a18626ef8f29c6cd473a621a8a4f8edb4
Johnny Guo <guozj94@gmail.com>
2017-02-04 20:27:13 -0500

    functional calculator

--- hw3/calculator/views.py
+++ hw3/calculator/views.py
@@ -2,74 +2,113 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-prev_val = 0
-curr_val = 0
-prev_operator = '+'
-display = 0
-is_equal = False
-equation = []
-double_operator = False
 
 def home_page(request):
 	display_val = 0
-	return render(request, 'calculator/calculator.html', {'display_val': 0})
+	context = {}
+	context['prev_val'] = 0
+	context['curr_val'] = 0
+	context['prev_operator'] = '+'
+	context['display_val'] = 0
+	context['is_equal'] = False
+	context['double_operator'] = False
+	return render(request, 'calculator/calculator.html', context)
 	
+def add_to_context(prev_val, curr_val, prev_operator, is_equal, double_operator, display_val):
+	context = {}
+	context['prev_val'] = prev_val
+	context['curr_val'] = curr_val
+	context['prev_operator'] = prev_operator
+	context['is_equal'] = is_equal
+	context['double_operator'] = double_operator
+	context['display_val'] = display_val
+	return context
+
+
 def calculator(request):
-	global curr_val
-	global is_equal
-	global double_operator
-	global prev_operator
-	global prev_val
-	if 'number' in request.POST: #everytime you refresh create a post!
-		print request.POST['number']
+
+	prev_val = request.POST['prev_val']
+	curr_val = request.POST['curr_val']
+	prev_operator = request.POST['prev_operator']
+	is_equal = request.POST['is_equal']
+	double_operator = request.POST['double_operator']
+
+	try:		
+		if isinstance(int(prev_val), int) and isinstance(int(curr_val), int) and (prev_operator in ['+', '-', '*', '/']) and (is_equal in ['True', 'False']) and (double_operator in ['True', 'False']):
+			prev_val = int(prev_val)
+			curr_val = int(curr_val)			
+	except:
+		return render(request, 'calculator/calculator.html', {'display_val': 'ERROR!'})
+
+	print request.POST.get('number', False) in ['0','1','2','3','4','5','6','7','8','9']
+	if request.POST.get('number', False) in ['0','1','2','3','4','5','6','7','8','9']:
 		is_equal = False
 		double_operator = False
 		if curr_val == 0:
 			curr_val = int(request.POST['number'])
-			print "one number"
-			return render(request, 'calculator/calculator.html', {'display_val': curr_val, 'curr_val': curr_val})
+			context = add_to_context(prev_val, curr_val, prev_operator, is_equal, double_operator, curr_val)
+			return render(request, 'calculator/calculator.html', context)
 		else:
-			curr_val = curr_val * 10 + int(request.POST['number'])
-			print "two or more number"
-			return render(request, 'calculator/calculator.html', {'display_val': curr_val, 'curr_val': curr_val})
-	
-	if ('sign' in request.POST) and (not double_operator): #if the input is '=', calculate and initialize the calculator
+			curr_val = int(curr_val) * 10 + int(request.POST['number'])
+			context = add_to_context(prev_val, curr_val, prev_operator, is_equal, double_operator, curr_val)
+			return render(request, 'calculator/calculator.html', context)
+
+	if (request.POST.get('sign', False) in ['+', '-', '*', '/']) and (double_operator == 'False'):
 		if prev_operator == '+':
 			result = prev_val + curr_val
 			prev_operator = request.POST['sign']
 			prev_val = result
 			curr_val = 0
-			return render(request, 'calculator/calculator.html', {'display_val': result, 'curr_val': curr_val, 'prev_val': prev_val, 'prev_operator': prev_operator})
+			context = add_to_context(prev_val, curr_val, prev_operator, is_equal, double_operator, result)
+			return render(request, 'calculator/calculator.html', context)
 		elif prev_operator == '-':
 			result = prev_val - curr_val
 			prev_operator = request.POST['sign']
 			prev_val = result
 			curr_val = 0
-			return render(request, 'calculator/calculator.html', {'display_val': result, 'curr_val': curr_val, 'prev_val': prev_val, 'prev_operator': prev_operator})
+			context = add_to_context(prev_val, curr_val, prev_operator, is_equal, double_operator, result)
+			return render(request, 'calculator/calculator.html', context)
 		elif prev_operator == '*':
 			result = prev_val * curr_val
 			prev_operator = request.POST['sign']
 			prev_val = result
 			curr_val = 0
-			return render(request, 'calculator/calculator.html', {'display_val': result, 'curr_val': curr_val, 'prev_val': prev_val, 'prev_operator': prev_operator})
+			context = add_to_context(prev_val, curr_val, prev_operator, is_equal, double_operator, result)
+			return render(request, 'calculator/calculator.html', context)
 		elif prev_operator == '/':
 			if curr_val == 0:
-				return render(request, 'calculator/calculator.html', {'display_val': 'Can not divide 0!', 'curr_val': 0, 'prev_val': 0, 'prev_operator': '+'})
+				context = add_to_context(0, 0, '+', False, False, 'Cannot divide by 0')
+				return render(request, 'calculator/calculator.html', context)
 			else:
-				result = int(prev_operator / curr_val)
+				result = int(prev_val / curr_val)
 				prev_operator = request.POST['sign']
 				prev_val = result
 				curr_val = 0
-				return render(request, 'calculator/calculator.html', {'display_val': result, 'curr_val': curr_val, 'prev_val': prev_val, 'prev_operator': prev_operator})
+				context = add_to_context(prev_val, curr_val, prev_operator, is_equal, double_operator, result)
+				return render(request, 'calculator/calculator.html', context)
 
-	if request.POST['sign'] == '=':
+	if request.POST.get('sign', False) == '=':
 		if prev_operator == '+':
 			result = prev_val + curr_val
-			prev_operator = '+'
-			prev_val = 0
-			curr_val = 0
-			return render(request, 'calculator/calculator.html', {'display_val': result, 'curr_val': 0, 'prev_val': 0, 'prev_operator': '+'})
-			
+			context = add_to_context(0, 0, '+', False, False, result)
+			return render(request, 'calculator/calculator.html', context)
+		if prev_operator == '-':
+			result = prev_val - curr_val
+			context = add_to_context(0, 0, '+', False, False, result)
+			return render(request, 'calculator/calculator.html', context)
+		if prev_operator == '*':
+			result = prev_val * curr_val
+			context = add_to_context(0, 0, '+', False, False, result)
+			return render(request, 'calculator/calculator.html', context)
+		if prev_operator == '/':
+			if curr_val == 0:
+				context = add_to_context(0, 0, '+', False, False, 'Cannot divide by 0')
+				return render(request, 'calculator/calculator.html', context)
+			else:
+				result = int(prev_val / curr_val)
+				context = add_to_context(0, 0, '+', False, False, result)
+				return render(request, 'calculator/calculator.html', context)
+
 
 	return render(request, 'calculator/calculator.html', {})
 
